b9dcdd3bff7def86e18853d12b9681bad1219b9d
koolnino <44924530+koolnino@users.noreply.github.com>
2025-05-11 23:37:17 +0200

    Add files via upload

--- brick_game.c
+++ brick_game.c
@@ -1,27 +1,22 @@
 #include <furi.h>
 #include <gui/gui.h>
 #include <gui/view_port.h>
-#include <furi_hal.h>
 
-static void draw_callback(Canvas* canvas, void* ctx) {
+static void draw(Canvas* canvas, void* ctx) {
     UNUSED(ctx);
     canvas_clear(canvas);
-    canvas_draw_str(canvas, 10, 30, "BrickGame is working!");
+    canvas_draw_str(canvas, 10, 30, "BrickGame OK");
 }
 
 int32_t brick_game_app(void* p) {
     UNUSED(p);
-
     Gui* gui = furi_record_open("gui");
-    ViewPort* view_port = view_port_alloc();
-    view_port_draw_callback_set(view_port, draw_callback, NULL);
-    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
-
+    ViewPort* vp = view_port_alloc();
+    view_port_draw_callback_set(vp, draw, NULL);
+    gui_add_view_port(gui, vp, GuiLayerFullscreen);
     furi_delay_ms(2000);
-
-    gui_remove_view_port(gui, view_port);
-    view_port_free(view_port);
+    gui_remove_view_port(gui, vp);
+    view_port_free(vp);
     furi_record_close("gui");
-
     return 0;
 }
